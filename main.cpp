b2a5a244c108e2cb894cc91b8b3686f793220c4a
bchooxg <56596358+bchooxg@users.noreply.github.com>
2022-01-26 14:36:01 +0800

    Added getters and setters

--- main.cpp
+++ main.cpp
@@ -2,6 +2,60 @@
 
 using namespace std;
 
+class ShapeTwoD{
+private:
+    string name;
+    bool containsWarpSpace;
+public:
+    // Public Constructors
+    ShapeTwoD(string name, bool containsWarpSpace){
+        this->name = name;
+        this->containsWarpSpace = containsWarpSpace;
+    }
+    // Getter Methods
+    string getName() const {
+        return name;
+    }
+
+    bool isContainsWarpSpace() const {
+        return containsWarpSpace;
+    }
+
+    virtual string toString(){
+        return "";
+    }
+    // Setter Methods
+    void setName(string name) {
+        this->name = name;
+    }
+
+    void setContainsWarpSpace(bool containsWarpSpace) {
+        this->containsWarpSpace = containsWarpSpace;
+    }
+    // Virtual Methods
+    virtual double computeArea()=0;
+    virtual bool isPointInShape(int x, int y)=0;
+    virtual bool isPointOnShape(int x, int y)=0;
+
+
+};
+
+class Cross{
+
+};
+
+class Square{
+
+};
+
+class Circle{
+
+};
+
+class Rectangle{
+
+};
+
 int main() {
     cout << "Hello, World!" << std::endl;
     return 0;
